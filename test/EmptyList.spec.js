b9394ae165688175cfe4587a67c8d1a24e4d413d
Mumin Cacao <mumincacao@gmail.com>
2021-05-30 18:26:38 +0900

    Remove meaningless test.

--- test/EmptyList.spec.js
+++
@@ -1,9 +0,0 @@
-import { mount, RouterLinkStub } from '@vue/test-utils'
-import EmptyList from '@/components/EmptyList'
-
-describe('EmptyList', () => {
-  test('is a Vue instance', () => {
-    const wrapper = mount(EmptyList, { stubs: { NuxtLink: RouterLinkStub } })
-    expect(wrapper.vm).toBeTruthy()
-  })
-})
